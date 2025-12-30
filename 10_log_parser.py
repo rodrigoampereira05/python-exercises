4f8cfb05b86a128d6f18acd93e4e4a944354a229
Rodrigo Pereira <rodrigoampereira05@gmail.com>
2025-12-30 14:29:45 +0000

    Added updates to Mini-project

--- 10_log_parser.py
+++ 10_log_parser.py
@@ -1,4 +1,4 @@
-with open("access.log", "w") as file:
+"""with open("access.log", "w") as file:
     file.write("192.168.1.1 - GET /index.html\n")
     file.write("192.168.1.2 - GET /about.html\n")
     file.write("192.168.1.1 - POST /login\n")
@@ -6,31 +6,48 @@ with open("access.log", "w") as file:
     file.write("192.168.1.1 - GET /dashboard\n")
     file.write("192.168.1.2 - GET /contact.html\n")
     file.write("10.0.0.5 - POST /submit\n")
-    file.write("192.168.1.1 - GET /logoutn")
+    file.write("192.168.1.1 - GET /logoutn")"""
 
-print("\n")
-print("\n")
-print("\n")
 
-with open("access.log", "r") as file:
+"""with open("access.log", "r") as file:
     content = file.read()
     print(content)
-
+"""
 contagem = {}
 
 def read_ips():
-    with open("access.log", "r") as file:
-        lines = file.readlines()
+    try:
+        with open("access.log", "r") as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        print("Ficheiro nao encontrado")
+        return False
 
 #vai separar a linha em por espaços
     for line in lines:
         parts = line.split(" ")
         ip = parts[0]
-        print(ip)
         if ip in contagem:
             contagem[ip] = contagem[ip] +1
         else:
             contagem[ip] = 1
-read_ips()
+    return True
 
-print(contagem)
+def ip_mais_usado():
+    maior_numero = 0
+    ip_maior = ""
+    
+    for chave, valor in contagem.items():
+        if valor > maior_numero:
+            maior_numero = valor
+            ip_maior = chave
+    
+    return ip_maior, maior_numero
+
+if read_ips():  
+    print(f"Os ips usados sao: {contagem}")
+
+
+    ip, numero = ip_mais_usado()
+    print(f"O ip mais usado foi: {ip} com {numero} acessos ")
+
