f7b289a4553e7fcc00e2472a58d8b59e11b90cff
Rodrigo Pereira <rodrigoampereira05@gmail.com>
2025-12-30 11:14:23 +0000

    Added variables and types exercise

--- 01_variables_and_types.py
+++ 01_variables_and_types.py
@@ -3,3 +3,34 @@ name = "Rodrigo"
 print(name)
 print(type(name))
 
+#Intergers
+age = 20
+print(age)
+print(type(age))
+
+#Float
+height = 1.82
+print(height)
+print(type(height))
+
+#Bool
+is_Goal = True #or False
+print(is_Goal)
+print(type(is_Goal))
+
+#List
+cars = ["BMW", "Mercedes", "Mini"]
+numbers = [1.2, 3, 10]
+print(cars)
+print(numbers)
+print(type(cars))
+print(type(numbers))
+
+#Dict
+users = {
+    "user1": 123,
+    "user2": 456
+}
+
+print(users)
+print(type(users))
